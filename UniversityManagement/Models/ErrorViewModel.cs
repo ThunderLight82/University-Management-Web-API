d021cf98e94fe2a78671425a78cc93a783f905b0
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-12-02 13:29:47 +0200

    Initial creation of University Management project with integration of MVC in ASP.Net Core
    1) Created "Development" branch from "main" branch
    1) Added .gitingore.
    2) Created solution to hold future projects.

---
+++ UniversityManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniversityManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
