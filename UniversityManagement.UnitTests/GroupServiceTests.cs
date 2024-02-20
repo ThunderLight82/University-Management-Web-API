2969bb7a5e605a303a10b8d87c0da883a7c9eea8
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-20 14:31:14 +0200

    Also fix namespaces

--- UniversityManagement.UnitTests/GroupServiceTests.cs
+++ UniversityManagement.UnitTests/GroupServiceTests.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using UniversityManagement.Application.Interfaces;
 using UniversityManagement.Application.Services;
-using UniversityManagement.Application.Services.Interfaces;
 using UniversityManagement.Application.Validations;
 using UniversityManagement.DataAccess;
 using UniversityManagement.DTO.EntitiesDto;
--- UniversityManagement.UnitTests/StudentServiceTests.cs
+++ UniversityManagement.UnitTests/StudentServiceTests.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using UniversityManagement.Application.Interfaces;
 using UniversityManagement.Application.Services;
-using UniversityManagement.Application.Services.Interfaces;
 using UniversityManagement.Application.Validations;
 using UniversityManagement.Domain.Entities;
 using UniversityManagement.DataAccess;
